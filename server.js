9674f289e4a95e0162930c34ba97b8afe3b8bb8f
moti amar <moramar36@gmail.com>
2025-09-11 17:47:37 +0300

    API add

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { answerQuestion } = require('./index');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
